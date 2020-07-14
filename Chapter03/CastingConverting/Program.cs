61e57eb459b3e98dce53152fa0122829f2d1f4a3
vladar21 <vladar21@gmail.com>
2020-07-15 01:19:11 +0300

    14.07.2020 part of 3 chapter

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using static System.Console;
+using static System.Convert;
+
+namespace CastingConverting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = a; // тип int может быть сохранен как double
+            WriteLine(b);
+
+            // double c = 9.8;
+            // int d = c; // компилятор выдаст ошибку на данной строке
+            // WriteLine(d);
+            double c = 9.8;
+            int d = (int)c;
+            WriteLine(d); // d равняется 9 с потерей части .8
+
+            long e = 10;
+            int f = (int)e;
+            WriteLine($"e is {e} and f is {f}");
+            e = long.MaxValue;
+            f = (int)e;
+            WriteLine($"e is {e} and f is {f}");
+
+            double g = 9.8;
+            int h = ToInt32(g);
+            WriteLine($"g is {g} and h is {h}");
+
+            double i = 9.49;
+            double j = 9.5;
+            double k = 10.49;
+            double l = 10.5;
+            WriteLine($"i is {i}, ToInt(i) is {ToInt32(i)}");
+            WriteLine($"j is {j}, ToInt(j) is {ToInt32(j)}");
+            WriteLine($"k is {k}, ToInt(k) is {ToInt32(k)}");
+            WriteLine($"l is {l}, ToInt(l) is {ToInt32(l)}");
+
+            int number = 12;
+            WriteLine(number.ToString());
+            bool boolean = true;
+            WriteLine(boolean.ToString());
+            DateTime now = DateTime.Now;
+            WriteLine(now.ToString());
+            object me = new object();
+            WriteLine(me.ToString());
+
+            // конвертация бинарного объекта в строку
+            // выделить массив из 128 байт
+            byte[] binaryObject = new byte[128];
+            // заполнить массив случайными байтами
+            (new Random()).NextBytes(binaryObject);
+            WriteLine("Binary Object as bytes:");
+            for (int index = 0; index < binaryObject.Length; index++)
+            {
+                Write($"{binaryObject[index]:X} ");
+            }
+            WriteLine();
+            // преобразовать в строку Base64
+            string encoded = Convert.ToBase64String(binaryObject);
+            WriteLine($"Binary Object as Base64: {encoded}");
+
+            // разбор строки
+            int age = int.Parse("38");
+            DateTime birthday = DateTime.Parse("4 July 1980");
+            WriteLine($"I was born {age} years ago.");
+            WriteLine($"My birthday is {birthday}.");
+            WriteLine($"My birthday is {birthday:D}.");
+
+            // int count = int.Parse("abc"); // ошибка, не может преобразовать неккоректный формат
+            Write("How many eggs are there? ");
+            int count;
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out count))
+            {
+                WriteLine($"There are {count} eggs.");
+            }
+            else
+            {
+                WriteLine("I could not parse the input.");
+            }
+        }
+    }
+}
