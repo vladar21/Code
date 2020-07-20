b506b3cb6010f6c63e3d5f984940a4e2349282b0
vladar21 <vlad.rastvorov@aol.com>
2020-07-20 11:27:57 +0300

    work

---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using static System.Console;
+
+namespace WritingFunctions
+{
+    class Program
+    {
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table");
+            for (int row = 1; row <= 12; row++)
+            {
+                WriteLine($"{row} x {number} = {row * number}");
+            }
+        }
+
+        static void RunTimesTable()
+        {
+            Write("Enter a number between 0 and 255: ");
+            if (byte.TryParse(ReadLine(), out byte number))
+            {
+                TimesTable(number);
+            }
+            else
+            {
+                WriteLine("You did not enter a valid number!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RunTimesTable();
+        }
+    }
+}
